d081800a5923517bd91db2fe1e20acc641ff3d32
Владислав <92315082+Vladislavrudnev@users.noreply.github.com>
2022-09-26 07:56:03 +0600

    Initial Commit

---
+++ Passing Data/Passing_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Passing_DataApp.swift
+//  Passing Data
+//
+//  Created by владислав Руднев on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Passing_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
